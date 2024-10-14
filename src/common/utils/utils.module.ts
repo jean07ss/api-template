df5f5295bd87b352c4731d933ea40374d7a9b67d
jeann07ss <jeann07ss@gmail.com>
2024-10-14 20:25:05 -0300

    fix: fix dependencies

--- src/common/utils/utils.module.ts
+++ src/common/utils/utils.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { GlobalExceptionFilter } from '@/common/utils/filters/global-exception.filter';
+import { LogInterceptor } from '@/common/utils/interceptors/log.interceptor';
 
 @Module({
   imports: [],
   controllers: [],
-  providers: [GlobalExceptionFilter],
+  providers: [GlobalExceptionFilter, LogInterceptor],
   exports: [],
 })
 export class UtilsModule {}
